f8b4fd5bcb264de145dcd821e6e270c6af28c783
AKSHAY PRASAD HONNAVALLI <93462091+Akshay-Honnavalli@users.noreply.github.com>
2024-03-06 22:26:50 +0530

    Create hello2.cpp

---
+++ hello2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "this is the hello2.cpp file\n";
+  cout << "Hello, World2\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
